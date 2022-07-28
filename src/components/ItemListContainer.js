5cb5e9e61096e43cb48ded3ac684e0d45ad67f1d
leomazzaferro <leonel.mazzaferro@gmail.com>
2022-07-28 00:20:52 -0300

    data

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,34 +1,12 @@
 
 import React , {useEffect, useState} from 'react';
-
 import ItemList from './ItemList';
-
+import {data} from '../mocks/DataApi'
 
 function ItemListContainer({greeting}) {
 
     const [listaProductos, setListaProductos] = useState([])
 
-
-    const productos = [
-        {id:'01', name:'remera', description:"Lorem ipsum dolor sit amet", price:1250, img:'https://picsum.photos/200', stock:5,},
-        {id:'02', name:'pantalon', description:"Lorem ipsum dolor sit amet", price:2300, img:'https://picsum.photos/201', stock:15},
-        {id:'03', name:'campera', description:"Lorem ipsum dolor sit amet", price:3800, img:'https://picsum.photos/202', stock:7},
-        {id:'04', name:'buzo', description:"Lorem ipsum dolor sit amet", price:2900, img:'https://picsum.photos/203', stock:3},
-        ]
-    const data = new Promise((resolve, reject) => {
-    
-            let condition = true
-                setTimeout(()=> {
-            if(condition){
-                    resolve(productos)
-            }else{
-                    reject('')
-            }}, 2000)
-            
-        })
-
-    
-    
 
     useEffect (()=>{
         data
